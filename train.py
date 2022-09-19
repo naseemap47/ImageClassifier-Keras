ceb28c37377d60bbfb750fb69f2eadc3dc9d2299
NASEEM A P <naseemalassampattil@gmail.com>
2022-09-19 13:06:58 +0530

    added print comments

--- train.py
+++ train.py
@@ -26,45 +26,53 @@ batch_size = args['batch_size']
 epochs = args['epochs']
 model_path = args['model']
 
-
-# All image data into a single list
-img_list, class_list, num_class = data_to_list(path_to_dir, img_size)
-
-# Split Data
-x_train, x_val, y_train, y_val = train_test_split(img_list, class_list, test_size=0.2)
-
-# Preprocessing
-train_generators, val_generators = create_generators(
-                                                    batch_size*2, num_class,
-                                                    x_train, y_train,
-                                                    x_val, y_val
-                                                )
-
-# Callbacks
-early_stopping = EarlyStopping(
-    min_delta=0.001,
-    patience=10,
-    mode='min',
-    restore_best_weights=True,
-    verbose=1
-)
-
-# Model
-model = Model(num_class, img_size)
-model.compile(
-    optimizer='adam',
-    loss='categorical_crossentropy',
-    metrics=['accuracy']
-)
-history = model.fit(
-    train_generators,
-    batch_size=batch_size,
-    epochs=epochs,
-    validation_data=val_generators,
-    callbacks=[early_stopping]
-)
-
 if os.path.isfile(model_path) is False:
+    # All image data into a single list
+    print('[INFO] Image Data Extraction Started...')
+    img_list, class_list, num_class = data_to_list(path_to_dir, img_size)
+    print('[INFO] Image Data Extraction Completed...')
+
+    # Split Data
+    x_train, x_val, y_train, y_val = train_test_split(img_list, class_list, test_size=0.2)
+
+    # Preprocessing
+    print('[INFO] Image Data Preprocessing Started...')
+    train_generators, val_generators = create_generators(
+                                                        batch_size*2, num_class,
+                                                        x_train, y_train,
+                                                        x_val, y_val
+                                                    )
+    print('[INFO] Image Data Preprocessing Completed...')
+
+    # Callbacks
+    early_stopping = EarlyStopping(
+        min_delta=0.001,
+        patience=10,
+        mode='min',
+        restore_best_weights=True,
+        verbose=1
+    )
+
+    # Model
+    print('[INFO] Model Training Started...')
+    model = Model(num_class, img_size)
+    model.compile(
+        optimizer='adam',
+        loss='categorical_crossentropy',
+        metrics=['accuracy']
+    )
+    history = model.fit(
+        train_generators,
+        batch_size=batch_size,
+        epochs=epochs,
+        validation_data=val_generators,
+        callbacks=[early_stopping]
+    )
+    print('[INFO] Model Training Completed...')
+
+    # Saved Model
     model.save(model_path)
     print(f'[INFO] Successfully Saved model in {model_path}')
 
+else:
+    print(f'[INFO] {model_path} is already Exist')
