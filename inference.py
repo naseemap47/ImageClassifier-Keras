e168788396378d6b51f0781be6731361d3034777
NASEEM A P <naseemalassampattil@gmail.com>
2022-09-20 10:26:55 +0530

    added inference on image data

--- inference.py
+++ inference.py
@@ -56,3 +56,34 @@ while True:
     if cv2.waitKey(1) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
+
+###### Image ######
+
+img_og = cv2.imread('Dataset/Bicycles crossing/29_10561_1577671999.1237395.png')
+img_resize = cv2.resize(img_og, (img_size, img_size))
+img = img_resize.astype('float32') / 255
+img = tf.keras.preprocessing.image.img_to_array(img)
+img = np.expand_dims(img, axis=0)
+
+
+prediction = model.predict(img)[0]
+predict = class_names[prediction.argmax()]
+# print(predict)
+prob_value = np.amax(prediction)
+if prob_value > threshold:
+    cv2.putText(img_og, "CLASS: ", (20, 35),
+                cv2.FONT_HERSHEY_PLAIN, 1.5,
+                (255, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(img_og, "PROBABILITY: ", (20, 75),
+                cv2.FONT_HERSHEY_PLAIN, 1.5,
+                (255, 0, 255), 2, cv2.LINE_AA)
+    cv2.putText(img_og, str(predict), (120, 35),
+                cv2.FONT_HERSHEY_PLAIN, 1.5,
+                (0, 255, 0), 2, cv2.LINE_AA)
+    cv2.putText(img_og, str(round(prob_value * 100, 2)) + "%",
+                (180, 75), cv2.FONT_HERSHEY_PLAIN, 1.5,
+                (0, 255, 0), 2, cv2.LINE_AA)
+
+cv2.imshow('Webcam', img_og)
+if cv2.waitKey(0) & 0xFF == ord('q'):
+    cv2.destroyAllWindows()
