99b9a54ad299efab0ab39126dda64cee5964013c
NASEEM A P <naseemalassampattil@gmail.com>
2022-09-20 11:54:36 +0530

    fixed error with save image, given wrong img, added font size and thickness according to size of image, video and video

--- inference.py
+++ inference.py
@@ -36,6 +36,7 @@ class_names = sorted(os.listdir(path_to_data))
 if source.endswith(('.jpg', '.jpeg', '.png')):
     path_to_img = source
     img_og = cv2.imread(path_to_img)
+    h, w, _ = img_og.shape
     img_resize = cv2.resize(img_og, (img_size, img_size))
     img = img_resize.astype('float32') / 255
     img = tf.keras.preprocessing.image.img_to_array(img)
@@ -45,26 +46,42 @@ if source.endswith(('.jpg', '.jpeg', '.png')):
     predict = class_names[prediction.argmax()]
     # print(predict)
     prob_value = np.amax(prediction)
+
+    # if resize is less than original size
+    if img_size>h:
+        img_og = img_resize
+        h, w, _ = img_og.shape
+
     if prob_value > threshold:
-        cv2.putText(img_og, "CLASS: ", (20, 35),
-                    cv2.FONT_HERSHEY_PLAIN, 1.5,
-                    (255, 0, 255), 2, cv2.LINE_AA)
-        cv2.putText(img_og, "PROBABILITY: ", (20, 75),
-                    cv2.FONT_HERSHEY_PLAIN, 1.5,
-                    (255, 0, 255), 2, cv2.LINE_AA)
-        cv2.putText(img_og, str(predict), (120, 35),
-                    cv2.FONT_HERSHEY_PLAIN, 1.5,
-                    (0, 255, 0), 2, cv2.LINE_AA)
+
+        x_axis = int(w/11.2)
+        y1 = int(h/6.4)
+        y2 = int(h/3)
+        y3 = int(h/4)
+        y4 = int(h/2.35)
+
+        font_size = int(((h+w)/2)/224)
+        font_thickness = int(font_size*2)
+        
+        cv2.putText(img_og, "CLASS: ", (x_axis, y1),
+                    cv2.FONT_HERSHEY_PLAIN, font_size,
+                    (255, 0, 255), font_thickness, cv2.LINE_AA)
+        cv2.putText(img_og, "PROBABILITY: ", (x_axis, y2),
+                    cv2.FONT_HERSHEY_PLAIN, font_size,
+                    (255, 0, 255), font_thickness, cv2.LINE_AA)
+        cv2.putText(img_og, str(predict), (x_axis, y3),
+                    cv2.FONT_HERSHEY_PLAIN, font_size,
+                    (0, 255, 0), font_thickness, cv2.LINE_AA)
         cv2.putText(img_og, str(round(prob_value * 100, 2)) + "%",
-                    (180, 75), cv2.FONT_HERSHEY_PLAIN, 1.5,
-                    (0, 255, 0), 2, cv2.LINE_AA)
+                    (x_axis, y4), cv2.FONT_HERSHEY_PLAIN, font_size,
+                    (0, 255, 0), font_thickness, cv2.LINE_AA)
 
     if save:
         os.makedirs('ImageOutput', exist_ok=True)
         img_full_name = os.path.split(path_to_img)[1]
         img_name = os.path.splitext(img_full_name)[0]
         path_to_save_img = f'ImageOutput/{img_name}.jpg'
-        cv2.imwrite(f'{path_to_save_img}', img)
+        cv2.imwrite(f'{path_to_save_img}', img_og)
         print(f'[INFO] Output Image Saved in {path_to_save_img}')
 
     cv2.imshow('Webcam', img_og)
@@ -94,8 +111,8 @@ else:
         if not success:
             print('[ERROR] Failed to Read Video feed')
             break
-        img = cv2.resize(img_og, (img_size, img_size))
-        img = img.astype('float32') / 255
+        img_resize = cv2.resize(img_og, (img_size, img_size))
+        img = img_resize.astype('float32') / 255
         img = tf.keras.preprocessing.image.img_to_array(img)
         img = np.expand_dims(img, axis=0)
 
@@ -104,7 +121,20 @@ else:
         predict = class_names[prediction.argmax()]
         # print(predict)
         prob_value = np.amax(prediction)
+
+        # if resize is less than original size
+        if img_size>h:
+            img_og = img_resize
+            h, w, _ = img_og.shape
+
         if prob_value > threshold:
+
+            x_axis = int(w/11.2)
+            y1 = int(h/6.4)
+            y2 = int(h/3)
+            y3 = int(h/4)
+            y4 = int(h/2.35)
+
             cv2.putText(img_og, "CLASS: ", (20, 35),
                         cv2.FONT_HERSHEY_PLAIN, 1.5,
                         (255, 0, 255), 2, cv2.LINE_AA)
