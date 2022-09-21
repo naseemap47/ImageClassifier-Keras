c03fa137a2a63c49f9e4c467930945578f39fc5b
NASEEM A P <naseemalassampattil@gmail.com>
2022-09-21 16:02:38 +0530

    when put class names as index numbes, model learning more faster than, put class names as names - after converting into categorical

--- utils.py
+++ utils.py
@@ -3,51 +3,24 @@ from keras.utils.np_utils import to_categorical
 import cv2
 import numpy as np
 import os
-import glob
 
 
-# def data_to_list(path_to_data, img_size):
-#     images = []
-#     class_var = []
-#     class_name_list = os.listdir(path_to_data)
-#     class_name_list = sorted(class_name_list)
-#     num_class = len(class_name_list)
-
-#     for class_name in class_name_list:
-#         img_path_list = glob.glob(os.path.join(path_to_data, class_name) + '/*.jpg') + \
-#             glob.glob(os.path.join(path_to_data, class_name) + '/*.jpeg') + \
-#             glob.glob(os.path.join(path_to_data, class_name) + '/*.png')
-
-#         for img_path in img_path_list:
-#             img = cv2.imread(img_path)
-#             img = cv2.resize(img, (img_size, img_size))
-#             images.append(img)
-#             class_var.append(class_name)
-
-#         print(f'[INFO] Extracted: {class_name}')
-
-#     images = np.array(images)
-#     class_var = np.array(class_var)
-
-#     # class names to categorical
-#     class_var = np.unique(class_var, return_inverse=True)[1]
-
-#     return images, class_var, num_class
-
+f = open('classes.txt')
+class_names = f.read().splitlines()
 
 def data_to_list(path_to_data, img_size):
     images = []
     class_no = []
     list_class = os.listdir(path_to_data)
     num_class = len(list_class)
-    for x in range(0, len(list_class)):
+    for x in range(0, num_class):
         img_list = os.listdir(os.path.join(path_to_data, str(x)))
-        print(x, end=' ')
         for y in img_list:
             img = cv2.imread(os.path.join(path_to_data, str(x), y))
             img = cv2.resize(img, (img_size, img_size))
             images.append(img)
             class_no.append(x)
+        print(f'[INFO] Extracted Class: {class_names[x]}')
     images = np.array(images)
     class_no = np.array(class_no)
     return images, class_no, num_class
