e9719b85f401689b69c8de7fcdfddfec9f026239
NASEEM A P <naseemalassampattil@gmail.com>
2022-09-21 16:38:41 +0530

    updated custom model, it will create model according to input image size

--- Models.py
+++ Models.py
@@ -6,20 +6,20 @@ from keras import Model
 def custom_model(no_classes, img_size):
     my_input = layers.Input(shape=(img_size, img_size, 3))
 
-    x = layers.Conv2D(32, (3, 3), activation='relu')(my_input)
+    x = layers.Conv2D(img_size, (3, 3), activation='relu')(my_input)
     x = layers.MaxPool2D()(x)
     x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(64, (3, 3), activation='relu')(x)
+    x = layers.Conv2D(img_size*2, (3, 3), activation='relu')(x)
     x = layers.MaxPool2D()(x)
     x = layers.BatchNormalization()(x)
 
-    x = layers.Conv2D(128, (3, 3), activation='relu')(x)
+    x = layers.Conv2D(img_size*4, (3, 3), activation='relu')(x)
     x = layers.MaxPool2D()(x)
     x = layers.BatchNormalization()(x)
 
     x = layers.GlobalAvgPool2D()(x)
-    x = layers.Dense(128, activation='relu')(x)
+    x = layers.Dense(img_size*2, activation='relu')(x)
     x = layers.Dense(no_classes, activation='softmax')(x)
 
     return Model(inputs=my_input, outputs=x)
